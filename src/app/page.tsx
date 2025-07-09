340b01d7eb3979b462e5027fcfb3b8a84ac8bcc2
Shawn Charles <62077627+CharlesCreativeContent@users.noreply.github.com>
2025-07-09 15:05:59 -0700

    Add files via upload
    Updated Crayon packages and added custom chart types to demo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,33 +2,29 @@
 
 import { C1Chat, ThemeProvider } from "@thesysai/genui-sdk";
 import "@crayonai/react-ui/styles/index.css";
-import { themePresets } from '@crayonai/react-ui';
+import { Theme, themePresets } from '@crayonai/react-ui';
 
-interface ChartColorPalette {
-  defaultChartPalette?: string[]; // Fallback for all charts
-  barChartPalette?: string[]; // Specific to BarChart
-  lineChartPalette?: string[]; // Specific to LineChart
-  areaChartPalette?: string[]; // Specific to AreaChart
-  pieChartPalette?: string[]; // Specific to PieChart
-  radarChartPalette?: string[]; // Specific to RadarChart
-  radialChartPalette?: string[]; // Specific to RadialChart
-}
+
+const lightTheme: Theme = {
+  // Professional light theme colors
+  defaultChartPalette: ["#4BCC00", "#0D1217", "green", "#A6EF5F", "light-green",],
+  pieChartPalette: ["#4BCC00", "blue", "green", "#A6EF5F", "light-green",],
+  barChartPalette: ["#4BCC00", "#0D1217", "green", "#A6EF5F", "#F7931A",],
+  lineChartPalette: ["#4BCC00", "#0D1217", "green", "#A6EF5F", "#F7931A",],
+  areaChartPalette: ["#4BCC00", "#0D1217", "green", "#A6EF5F", "#F7931A",],
+  radarChartPalette: ["#4BCC00", "#0D1217", "green", "#A6EF5F", "#F7931A",],
+  radialChartPalette: ["#4BCC00", "#0D1217", "green", "#A6EF5F", "#F7931A",],
+};
 
 export default function Home() {
   return (
     
-    <C1Chat agentName="CoinGecko + C1" apiUrl="/api/chat" 
+    <C1Chat  agentName="CoinGecko + C1" apiUrl="/api/chat" 
     logoUrl='https://static.coingecko.com/gecko-logo-new-color.svg'
-    mode="light"
-    theme={{ ...themePresets.jade,
-      defaultChartPalette: [
-        "#34495e",
-        "#16a085",
-        "#f39c12",
-        "#e74c3c",
-        "#8e44ad",
-      ],
-  } } 
+    theme={{
+      mode: 'light',
+      theme: lightTheme,
+    }}
      />
   );
 }
